746aec8aec5b4f29409e37da1750ab7a32ad4446
Abdulrahman-Alkhalaileh <abdulrahman.alkhalaileh@gmail.com>
2023-09-02 17:56:26 +0300

    finished backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log('Server...')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const {errorHandler} = require('./middleware/errorMiddleware') 
+const PORT = process.env.PORT || 5000
+
+//Connect to DB
+connectDB()
+
+const app = express()
+
+//middleware for the body 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res)=>{
+    res.json({
+        message:'welcome to support desk'
+    })
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, ()=> console.log(`Server is started on port ${PORT}`))
